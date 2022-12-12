a526b6243d92dda63a23803141a069896882e16b
Matthias De Smet <11850640+matthdsm@users.noreply.github.com>
2022-12-12 08:35:08 +0100

    fix python script

--- bin/create_cumulative_region_dist.py
+++ bin/create_cumulative_region_dist.py
@@ -11,17 +11,17 @@ if __name__ == '__main__':
     optparser = OptionParser(usage="Usage:{} --bed coverage.per-base.bed".format(__file__))
     optparser.add_option('-b', '--bed', help='Per base coverage bed for panel regions', action='store')
     (options, args) = optparser.parse_args()
-    if not options.bed:   # if SeqCap_run is not given
+    if not options.bed:
         optparser.error('NO BED FILE GIVEN!')
 
     # create dataframe
-    df = pd.read_csv(options.bed, sep="\t", names = ["chr","start","stop","depth"])
+    df = pd.read_csv(options.bed, sep="\t", names=["chr", "start", "stop", "depth"])
 
     # calculate length of region
-    df['length']=df['stop']-df['start']
+    df['length']=df['stop'] - df['start']
 
     # sum length with equal depth
-    df2 = pd.DataFrame(data = df.groupby('depth')['length'].sum())
+    df2 = pd.DataFrame(data=df.groupby('depth')['length'].sum())
 
     # sort by depth
     df2 = df2.sort_values(by='depth', ascending=False)
@@ -31,7 +31,7 @@ if __name__ == '__main__':
 
     # print output + add missing values for every depth
     # 3 numbers after comma (= more specific than real mosdepth dist.txt file)
-    for value in list(reversed(range(0, df2.index.tolist()[0]+1,1))):
+    for value in list(reversed(range(0, df2.index.tolist()[0] + 1, 1))):
         try:
             running_cum_percent = df2.loc[value, 'cum_percent']
             print(f'total\t{value}\t{running_cum_percent:.3f}')
