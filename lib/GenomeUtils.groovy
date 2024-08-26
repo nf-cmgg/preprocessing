b24a375bedfdd11ad14014d50cc57d16ebf2b077
Matthias De Smet <11850640+matthdsm@users.noreply.github.com>
2024-08-26 13:21:13 +0200

    simplify helper class

--- lib/GenomeUtils.groovy
+++ lib/GenomeUtils.groovy
@@ -2,17 +2,15 @@
 // This file holds several functions specific to the preprocessing workflow in the nf-cmgg/preprocessing pipeline
 //
 
-import nextflow.Nextflow
-
 class GenomeUtils {
     //
     // Get attribute from genome config file e.g. fasta
     //
-    public static Object getGenomeAttribute(genome, attribute) {
+    def getGenomeAttribute(genome, attribute) {
         if (genome.containsKey(attribute)) {
-            return Nextflow.file(genome[attribute], checkIfExists: true)
+            return nextflow.Nextflow.file(genome[attribute], checkIfExists: true)
         } else {
-            Nextflow.error("Genome config does not contain attribute ${attribute}")
+            nextflow.Nextflow.error("Genome config does not contain attribute ${attribute}")
         }
         return null
     }
